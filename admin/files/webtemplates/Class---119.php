8a4a0eef2fd6be51eb87cf77a0201c358569a774
freejoe76 <joe.murphy@gmail.com>
2014-08-21 09:52:56 -0600

    Finish up the class work

--- admin/files/webtemplates/Class---119.php
+++ admin/files/webtemplates/Class---119.php
@@ -4,6 +4,10 @@
 <VAR $rightSingleQuote = chr(38)."rsaquo;">
 <VAR $form_ClassID = intval($form_ClassID)>
 <VAR $form_Sport = intval($form_Sport)>
+<?PHP
+if ( isset($form_class_slug) ) $form_ClassID = class_id($form_class_slug, TRUE);
+if ( isset($form_sportslug) ) $form_SportID = sport_id($form_sportslug, TRUE);
+?>
 
 <IFEMPTY $form_Sport>
 <IFEMPTY $form_ClassID>
@@ -26,10 +30,6 @@
 <ELSE>
 ### Class Detail view ###
 ### This template displays when we've got a ClassID but no SportID ###
-<?PHP
-if ( isset($form_class_slug) ) $form_ClassID = class_id($form_class_slug, TRUE);
-if ( isset($form_sportslug) ) $form_SportID = sport_id($form_sportslug, TRUE);
-?>
 <QUERY name=Class prefix=Class CLASSID=$form_ClassID>
 <?PHP $sportslug = sport_id($form_Sport); ?>
 <div id="breadcrumbs">
@@ -62,9 +62,9 @@ $class_slug = class_id($Class_ClassID);
 
 <div id="breadcrumbs">
     <INCLUDE site=default tpl=TemplateBreadcrumbs>
-    {$rightSingleQuote} <a href="{$externalURL}site=default&tpl=Class&ClassID">Classes</a>
-    {$rightSingleQuote} <a href="{$externalURL}site=default&tpl=Class&ClassID={$form_ClassID}">{$Class_ClassName}</a>
-    {$rightSingleQuote} <a href="{$externalURL}site=default&tpl=Sport&Sport={$form_Sport}">{$Sport_SportName}</a>
+    {$rightSingleQuote} <a href="{$domainURL}/classes/">Classes</a>
+    {$rightSingleQuote} <a href="{$domainURL}/classes/{$class_slug}/">{$Class_ClassName}</a>
+    {$rightSingleQuote} <a href="{$domainURL}/sports/{$sportslug}/">{$Sport_SportName}</a>
 </div>
 <IFEQUAL $Sport_SportName "Ice Hockey">
 <h1>Colorado ###Class {$Class_ClassName}### Prep {$Sport_SportName} Leaders</h1>
