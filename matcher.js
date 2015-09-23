c6501a01cc8a6ea5174694d9ed51183188bdfbcb
freejoe76 <jmurphy@denverpost.com>
2015-09-23 16:55:28 -0400

    Start on making this lookup production-ready

--- matcher.js
+++ matcher.js
@@ -7,24 +7,32 @@ var lookup = {
   'Cherry Creek': 'http://preps.denverpost.com/schools/cherry-creek/10/',
   'The Denver Post': 'http://www.denverpost.com/'
 }
-var proper_nouns = new RegExp(/\b([A-Z][a-z]+)\s(([A-Z][a-z]+)\s?)+\b/gm);
-$('#articleBody p').each( function() { 
-  var results = $(this).text().match(proper_nouns);
 
-  if ( results !== null )
-  {
-    
-    var count = results.length;
-    for ( var i = 0; i < count; i++ )
-    {
-      if ( lookup.hasOwnProperty(results[i]) )
-      {
-        // Replace the first instance of the text with the linked text,
-        // then remove the lookup from the object so we don't link it again.
-        $(this).html($(this).html().replace(results[i], '<a href="' + lookup[results[i]] + '">' + results[i] + '</a>'));
-        delete(lookup[results[i]]);
-      }
+var matcher = {
+    'regex': new RegExp(/\b([A-Z][a-z]+)\s(([A-Z][a-z]+)\s?)+\b/gm),
+    'init': function () {
+        $.getScript("http://extras.denverpost.com/app/preps/prep_lookup.js", function()
+        {
+            $('#articleBody p').each( function() { 
+              var results = $(this).text().match(this.regex);
+
+              if ( results !== null )
+              {
+                
+                var count = results.length;
+                for ( var i = 0; i < count; i++ )
+                {
+                  if ( lookup.hasOwnProperty(results[i]) )
+                  {
+                    // Replace the first instance of the text with the linked text,
+                    // then remove the lookup from the object so we don't link it again.
+                    $(this).html($(this).html().replace(results[i], '<a href="' + lookup[results[i]] + '">' + results[i] + '</a>'));
+                    delete(lookup[results[i]]);
+                  }
+                }
+              }
+
+            });
+        });
     }
-  }
-
-});
+}
