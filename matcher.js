b15f37be3e7d553734cf99041d0184cf008210d2
freejoe76 <jmurphy@denverpost.com>
2015-09-23 16:45:11 -0400

    Initial add of the proper noun matcher

---
+++ matcher.js
@@ -0,0 +1,30 @@
+// Look up proper nouns in an article against a lookup table of proper nouns to URLs.
+// If there's a match, link the proper noun to the URL.
+// Uses jQuery.
+
+var lookup = {
+  'Kanye West': 'http://kanyewest.com/',
+  'Cherry Creek': 'http://preps.denverpost.com/schools/cherry-creek/10/',
+  'The Denver Post': 'http://www.denverpost.com/'
+}
+var proper_nouns = new RegExp(/\b([A-Z][a-z]+)\s(([A-Z][a-z]+)\s?)+\b/gm);
+$('#articleBody p').each( function() { 
+  var results = $(this).text().match(proper_nouns);
+
+  if ( results !== null )
+  {
+    
+    var count = results.length;
+    for ( var i = 0; i < count; i++ )
+    {
+      if ( lookup.hasOwnProperty(results[i]) )
+      {
+        // Replace the first instance of the text with the linked text,
+        // then remove the lookup from the object so we don't link it again.
+        $(this).html($(this).html().replace(results[i], '<a href="' + lookup[results[i]] + '">' + results[i] + '</a>'));
+        delete(lookup[results[i]]);
+      }
+    }
+  }
+
+});
--- update-inarticle-js.bash
+++ update-inarticle-js.bash
@@ -2,3 +2,8 @@
 # Update the preps js that allows for auto-linking of proper nouns.
 wget "http://preps.denverpost.com/js.php?include=school" -O schools.js
 python deletecomma.py `tr -d '\n\r' < schools.js` > school.js
+
+wget "http://preps.denverpost.com/js.php?include=player" -O players.js 
+python deletecomma.py `tr -d '\n\r' < players.js` > player.js 
+
+cat school.js player.js > prep_lookup.js
