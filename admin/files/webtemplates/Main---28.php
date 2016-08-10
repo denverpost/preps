8257af83e83f57907161ca1c04829b62dd165ffc
freejoe76 <jmurphy@denverpost.com>
2016-08-10 11:05:13 -0600

    Fix dates

--- admin/files/webtemplates/Main---28.php
+++ admin/files/webtemplates/Main---28.php
@@ -1,9 +1,9 @@
 <VAR $domainURL = "http://preps.denverpost.com">
 <?PHP $strNow = time() ?>
 ###YEARCHECK###
-<VAR $fallTime = strtotime("10 August 2015")>
-<VAR $midNovemberTime = strtotime("15 November 2015")>
-<VAR $winterTime = strtotime("30 November 2015")>
+<VAR $fallTime = strtotime("10 August 2016")>
+<VAR $midNovemberTime = strtotime("15 November 2016")>
+<VAR $winterTime = strtotime("30 November 2016")>
 <VAR $springTime = strtotime("5 March 2017")>
 
 <VAR $externalURL = "http://preps.denverpost.com/home.html?">
